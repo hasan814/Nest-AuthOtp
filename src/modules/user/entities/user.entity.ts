c061e08c38edd9b9d110ec74087cdfe74d4b3953
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-15 21:48:32 +0330

    Added Sign up and Login Dto Class

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -9,7 +9,11 @@ export class UserEntity {
   first_name: string;
   @Column({ nullable: true })
   last_name: string;
-  @Column()
+  @Column({ nullable: true })
+  email: string;
+  @Column({ nullable: true })
+  password: string;
+  @Column({ nullable: true })
   mobile: string;
   @Column({ default: false })
   mobile_verify: boolean;
