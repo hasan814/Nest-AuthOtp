776ae64bd05b53b3fc96f482dc27957b5ce484d5
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-15 11:48:30 +0330

    Added UserEntity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { CustomConfigModule } from './config/configs.module';
 import { TypeOrmDbConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { Module } from '@nestjs/common';
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmDbConfig,
       inject: [TypeOrmDbConfig]
-    })],
+    }),
+    UserModule],
   controllers: [],
   providers: [TypeOrmDbConfig],
 })
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,6 +1,7 @@
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 import { ConfigService } from "@nestjs/config";
 import { Injectable } from "@nestjs/common";
+import { UserEntity } from "src/modules/user/entities/user.entity";
 
 
 @Injectable()
@@ -10,10 +11,13 @@ export class TypeOrmDbConfig implements TypeOrmOptionsFactory {
     return {
       type: "postgres",
       synchronize: true,
+      entities: [UserEntity],
+      autoLoadEntities: true,
       port: this.configService.get("Db.port"),
       host: this.configService.get("Db.host"),
       username: this.configService.get("Db.username"),
       password: this.configService.get("Db.password"),
+      database: this.configService.get("Db.database"),
     }
   }
 }
