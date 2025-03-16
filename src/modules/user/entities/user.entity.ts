8820bf9418099682bbe4acdc44d0bad297f6cf8c
Hasan Moosavi <h.mousavi910@gmail.com>
2025-03-17 00:02:14 +0330

    Added OTPEntity

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { OTPEntity } from "./otp.entity";
 
 @Entity("user")
 export class UserEntity {
@@ -16,4 +17,9 @@ export class UserEntity {
   created_at: Date
   @UpdateDateColumn()
   updated_at: Date
+  @Column()
+  otpId: number;
+  @OneToOne(() => OTPEntity, otp => otp.user)
+  @JoinColumn({ name: "otpId" })
+  otp: OTPEntity
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
 import { Module } from '@nestjs/common';
+import { OTPEntity } from './entities/otp.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, OTPEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
