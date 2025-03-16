2ce9473ee43a6c629d968b424788312a63b09b8c
Hasan Moosavi <h.mousavi910@gmail.com>
2025-03-16 21:00:16 +0330

    Review again

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,7 @@
-import { CustomConfigModule } from './config/configs.module';
-import { TypeOrmDbConfig } from './config/typeorm.config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './modules/user/user.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [
-    CustomConfigModule,
-    TypeOrmModule.forRootAsync({
-      useClass: TypeOrmDbConfig,
-      inject: [TypeOrmDbConfig]
-    }),
-    UserModule,
-    AuthModule,
-    JwtModule
-  ],
   controllers: [],
-  providers: [TypeOrmDbConfig],
+  providers: [],
 })
 export class AppModule { }
--- src/modules/auth/types/payload.ts
+++
@@ -1,4 +0,0 @@
-export type TTokensPayload = {
-  mobile: string;
-  id: number
-}
